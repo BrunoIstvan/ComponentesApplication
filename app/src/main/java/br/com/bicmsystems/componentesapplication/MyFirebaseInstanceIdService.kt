864ed3e9b29b167480f150d52885f29a6f8be1b2
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2018-10-09 23:03:48 -0300

    Notificação via Firebase

---
+++ app/src/main/java/br/com/bicmsystems/componentesapplication/MyFirebaseInstanceIdService.kt
@@ -0,0 +1,19 @@
+package br.com.bicmsystems.componentesapplication
+
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.FirebaseInstanceIdService
+
+class MyFirebaseInstanceIdService : FirebaseInstanceIdService() {
+
+    override fun onTokenRefresh() {
+
+        super.onTokenRefresh()
+
+        val token = FirebaseInstanceId.getInstance().token
+
+        Log.i("TOKEN", token)
+
+    }
+
+}
